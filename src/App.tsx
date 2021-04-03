a4abc8d21d0730392508b25eae23f809942c26a0
yar2nman <yar2nman@gmail.com>
2021-04-03 22:18:04 +0300

    user controller

--- src/App.tsx
+++ src/App.tsx
@@ -3,44 +3,67 @@ import React from 'react';
 import './App.css';
 
 import clsx from 'clsx'
-import { makeStyles } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Drawer from '@material-ui/core/Drawer'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 import AppMenu from './components/appMenue/AppMenu'
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
+import {UserList} from './components/userlist/userList';
 
 const App: React.FC = () => {
-  const classes = useStyles()
+  const classes = useStyles();
+  
+  const [showDrawer, setShowDrawer] = React.useState(false);
+
+  const toggleDrawer = (open: boolean) => 
+  (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+    setShowDrawer(open);
+  };
 
   return (
-    <div className={clsx('App', classes.root)}>
+    <Router>
+      <div className={clsx('App', classes.root)}>
       <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={toggleDrawer(true)}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Drawer
-        variant="permanent"
+        variant='temporary'
+        open={showDrawer}
+        onClose={toggleDrawer(false)}
         classes={{
           paper: classes.drawerPaper,
         }}
@@ -49,10 +72,30 @@ const App: React.FC = () => {
       </Drawer>
       <main className={classes.content}>
         <Container maxWidth="lg" className={classes.container}>
-          <Typography>I'm the content</Typography>
+          
+          {/* <Typography>I'm the content a</Typography> */}
+          <Switch>
+          <Route exact path="/">
+            {/* <Home /> */}Home
+          </Route>
+          <Route path="/about">
+            {/* <About /> */}About
+          </Route>
+          <Route path="/dashboard">
+            {/* <Dashboard /> */}Dashboard
+          </Route>
+          <Route path="/reports">
+            {/* <Dashboard /> */}Reports
+          </Route>
+          <Route path="/users">
+            <UserList users={[]} load={false}  />
+          </Route>
+        </Switch>
+
         </Container>
       </main>
     </div>
+    </Router>   
   )
 }
 
@@ -72,6 +115,7 @@ const useStyles = makeStyles(theme => ({
     color: '#fff',
   },
   content: {
+    marginTop: '35px',
     flexGrow: 1,
     height: '100vh',
     overflow: 'auto',
@@ -80,6 +124,20 @@ const useStyles = makeStyles(theme => ({
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
   },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - 0px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    // [theme.breakpoints.up('sm')]: {
+    //   display: 'none',
+    // },
+  }
 }))
+
+
 
 export default App;
