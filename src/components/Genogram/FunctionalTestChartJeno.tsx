d5cecc6222dcc46214c45d45ca2b8c73b654d71d
yar2nman <yar2nman@gmail.com>
2021-05-02 23:02:45 +0300

    handle change

--- src/components/Genogram/FunctionalTestChartJeno.tsx
+++ src/components/Genogram/FunctionalTestChartJeno.tsx
@@ -104,6 +104,97 @@ export const FunctionalChartJenoState = () => {
     const removedLinkKeys = obj.removedLinkKeys;
     const modifiedModelData = obj.modelData;
   };
+
+  // public handleModelChange(obj: go.IncrementalData) {
+  //   const insertedNodeKeys = obj.insertedNodeKeys;
+  //   const modifiedNodeData = obj.modifiedNodeData;
+  //   const removedNodeKeys = obj.removedNodeKeys;
+  //   const insertedLinkKeys = obj.insertedLinkKeys;
+  //   const modifiedLinkData = obj.modifiedLinkData;
+  //   const removedLinkKeys = obj.removedLinkKeys;
+  //   const modifiedModelData = obj.modelData;
+
+  //   // maintain maps of modified data so insertions don't need slow lookups
+  //   const modifiedNodeMap = new Map<go.Key, go.ObjectData>();
+  //   const modifiedLinkMap = new Map<go.Key, go.ObjectData>();
+  //   this.setState(
+  //     produce((draft: AppState) => {
+  //       let narr = draft.nodeDataArray;
+  //       if (modifiedNodeData) {
+  //         modifiedNodeData.forEach((nd: go.ObjectData) => {
+  //           modifiedNodeMap.set(nd.key, nd);
+  //           const idx = this.mapNodeKeyIdx.get(nd.key);
+  //           if (idx !== undefined && idx >= 0) {
+  //             narr[idx] = nd;
+  //             if (draft.selectedData && draft.selectedData.key === nd.key) {
+  //               draft.selectedData = nd;
+  //             }
+  //           }
+  //         });
+  //       }
+  //       if (insertedNodeKeys) {
+  //         insertedNodeKeys.forEach((key: go.Key) => {
+  //           const nd = modifiedNodeMap.get(key);
+  //           const idx = this.mapNodeKeyIdx.get(key);
+  //           if (nd && idx === undefined) {  // nodes won't be added if they already exist
+  //             this.mapNodeKeyIdx.set(nd.key, narr.length);
+  //             narr.push(nd);
+  //           }
+  //         });
+  //       }
+  //       if (removedNodeKeys) {
+  //         narr = narr.filter((nd: go.ObjectData) => {
+  //           if (removedNodeKeys.includes(nd.key)) {
+  //             return false;
+  //           }
+  //           return true;
+  //         });
+  //         draft.nodeDataArray = narr;
+  //         this.refreshNodeIndex(narr);
+  //       }
+
+  //       let larr = draft.linkDataArray;
+  //       if (modifiedLinkData) {
+  //         modifiedLinkData.forEach((ld: go.ObjectData) => {
+  //           modifiedLinkMap.set(ld.key, ld);
+  //           const idx = this.mapLinkKeyIdx.get(ld.key);
+  //           if (idx !== undefined && idx >= 0) {
+  //             larr[idx] = ld;
+  //             if (draft.selectedData && draft.selectedData.key === ld.key) {
+  //               draft.selectedData = ld;
+  //             }
+  //           }
+  //         });
+  //       }
+  //       if (insertedLinkKeys) {
+  //         insertedLinkKeys.forEach((key: go.Key) => {
+  //           const ld = modifiedLinkMap.get(key);
+  //           const idx = this.mapLinkKeyIdx.get(key);
+  //           if (ld && idx === undefined) {  // links won't be added if they already exist
+  //             this.mapLinkKeyIdx.set(ld.key, larr.length);
+  //             larr.push(ld);
+  //           }
+  //         });
+  //       }
+  //       if (removedLinkKeys) {
+  //         larr = larr.filter((ld: go.ObjectData) => {
+  //           if (removedLinkKeys.includes(ld.key)) {
+  //             return false;
+  //           }
+  //           return true;
+  //         });
+  //         draft.linkDataArray = larr;
+  //         this.refreshLinkIndex(larr);
+  //       }
+  //       // handle model data changes, for now just replacing with the supplied object
+  //       if (modifiedModelData) {
+  //         draft.modelData = modifiedModelData;
+  //       }
+  //       draft.skipsDiagramUpdate = true;  // the GoJS model already knows about these updates
+  //     })
+  //   );
+  // }
+
   const handleRelinkChange = (e: any) => {
     const target = e.target;
     const value = target.checked;
